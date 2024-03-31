8e97d81b3a46de5a36fb127ad910aa2852828dfa
Marcio Curvello <marciocurvello@gmail.com>
2024-03-31 10:32:51 -0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Marcio Curvello on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
